3661afabbebea79b48782cc58e9be6d87a5d6919
A7mDFaraj <a7mdfaraj@gmail.com>
2026-08-29 23:54:52 +0300

    chore: configure open-next and fix cloudflare build

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,11 +1,4 @@
-export default {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      incrementalCache: async () => (await import("@opennextjs/cloudflare/kv-cache")).default,
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-};
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig();
+
